d3b23d59dce468daae20178220fc5c9e707952d6
Mariobros888 <Thiago.lassabe12@gmail.com>
2026-02-24 15:41:30 -0300

    Update Dia1.js

--- Dia1.js
+++ Dia1.js
@@ -1,3 +1,6 @@
 Aprendi que las entradas <h1 a <h6 sirven para encaberzar la cosas de mayor a menos importancia
 Las entradas o comandos <p sirven para crear parafos, CSS se utiliza para darle mas estilos a las paginas añadiendo colores, imagenes etc. (decoracion) 
 JavaScript sirve para hacer la pagina mas interactiva añadiendo botones y para decirle a la pagina que hacer cuando alguien interactue con los botones (interfaz,interacciones) 
+Los comandos <img.> para colocar una imagen y no tiene una etiqueta de cierre como <h1> pero si tienen algo similar al ser <img scr= https://static.wikia.nocookie.net/oneshot/images/0/02/Niko.png/revision/latest?cb=20170401115609/>
+<Href=> hace funcionales a las URLs que tengan un Target (mas informacion a futuro sobre Target)
+scr=Es requerido para mostrar correctamente la imagen alt= sirve para dar informacion en caso de no poder ver la imagen correctamente (cuestiones de accesibilidad)
