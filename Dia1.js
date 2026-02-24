302f2dadff8ea2ce6e58ce4db5e27e09072b31c9
Mariobros888 <Thiago.lassabe12@gmail.com>
2026-02-24 14:50:13 -0300

    Create Dia1.js

---
+++ Dia1.js
@@ -0,0 +1,3 @@
+Aprendi que las entradas <h1 a <h6 sirven para encaberzar la cosas de mayor a menos importancia
+Las entradas o comandos <p sirven para crear parafos, CSS se utiliza para darle mas estilos a las paginas añadiendo colores, imagenes etc. (decoracion) 
+JavaScript sirve para hacer la pagina mas interactiva añadiendo botones y para decirle a la pagina que hacer cuando alguien interactue con los botones (interfaz,interacciones) 
